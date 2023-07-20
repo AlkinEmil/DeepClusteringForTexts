6ae7ee59e20a50303cc497a78a23a56b6f802df8
Alexander <44891804+stalex2902@users.noreply.github.com>
2023-07-20 12:06:44 +0300

    Add documentation

--- utils/training_and_visualisation.py
+++ utils/training_and_visualisation.py
@@ -1,11 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+import torch
+from torch.optim import Optimizer
+
 from tqdm.notebook import trange
 from IPython import display
+from typing import Dict, List
+
 import os
 
-
-def plot_losses(losses, title):
+def plot_losses(losses: List[Dict[str, List[float]]], title: List[str]) -> None:
+    '''Plot training losses for DeepClustering model.
+    
+        :param losses - list of losses dictionaries for vizualization
+        :param title - list of titles
+    '''
     if len(losses) == 2:
         assert len(losses) == len(title)
         fig, axs = plt.subplots(1, 2, figsize=(12, 4))
@@ -24,7 +34,19 @@ def plot_losses(losses, title):
         plt.legend()
     plt.show()
     
-def plot_clusters_with_centers(model, base_embeds, clusters=None, title="Deep Embedding Clustering for Banking77"):
+def plot_clusters_with_centers(
+    model,
+    base_embeds: torch.Tensor,
+    clusters: np.array = None,
+    title: str = "Deep Embedding Clustering for Banking77"
+) -> None:
+    '''Visualize predicted clusters and centroids.
+    
+        :param model - fitted TextClustering model
+        :param base_embeds - initial text embeddings to be clustered
+        :param clusters - cluster assignments
+        :param title - image title
+    '''
     device = model.centers.device
     centers = model.centers.cpu().detach().numpy()
     if clusters is None:
@@ -40,18 +62,38 @@ def plot_clusters_with_centers(model, base_embeds, clusters=None, title="Deep Em
     plt.legend()
     
     
-def train(model, data, optimizer, epochs, device="cpu", 
-          early_stoping=False, 
-          verbose=False, 
-          save_images=False, 
-          title="Deep Embedding Clustering for Banking77", 
-          save_dir='../imgs'):
+def train(model,
+          train_dataset: torch.Tensor,
+          optimizer: Optimizer,
+          epochs: int,
+          device: str = "cpu", 
+          early_stoping: bool = False, 
+          verbose: bool = False, 
+          save_images: bool = False, 
+          title: str = "Deep Embedding Clustering for Banking77", 
+          save_dir: str = '../imgs'
+         ) -> Dict[str, List[float]]:
+    '''Train DeepClustering model.
+    
+        :param model - DeepClustering model to be trained
+        :param train_dataset - tensor with initial text embeddings
+        :param optimizer - training optimizer
+        :param epochs - number of training epochs
+        :param device - if "cuda", use GPU for training
+        :param early_stoping - if True, use EarlyStopping
+        :param verbose - if True, show the images
+        :param save_images - if True, save the result images
+        :param title - title for images
+        :param save_dir - directory for result images to be saved
+        :return training losses
+    '''
     losses = dict()
     model.eval()
-    dataloader = model.create_dataloader(data)
-    _, init_clusters = model.transform_and_cluster(data.to(device))
+    dataloader = model.create_dataloader(train_dataset)
+    _, init_clusters = model.transform_and_cluster(train_dataset.to(device))
     
-    def update_losses(losses, loss):
+    def update_losses(losses: Dict[str, List[float]], loss: Dict[str: float]) -> None:
+        '''Utility for loss tracking.'''
         for i in loss:
             if losses.get(i) is None:
                 losses[i] = [loss[i].item()]
@@ -79,7 +121,10 @@ def train(model, data, optimizer, epochs, device="cpu",
                     plt.show()
                     display.clear_output(wait=True)
                 
-            if early_stoping and model.mode == "train_clusters" and np.mean(losses['clustering_loss'][-10:]) < np.mean(losses['geom_loss'][-10:]):
+            if (
+                early_stoping and model.mode == "train_clusters" and
+                np.mean(losses['clustering_loss'][-10:]) < np.mean(losses['geom_loss'][-10:])
+            ):
                 return losses
             t += 1
     return losses
